248b556267e77313ee4b476e5e67d17e6ff03224
Robin Murumets <99771771+robinmurumets@users.noreply.github.com>
2023-12-22 00:48:16 +0000

    Muudatus

--- app.py
+++ app.py
@@ -1,47 +1,110 @@
-import io
 import tkinter as tk
-from loogika import nimi, hind, fetch_html_sisu_ja_soup, transport, toote_url
+from tkinter import Canvas, Scrollbar, Toplevel
+from PIL import Image, ImageTk
+import requests
+import io
+from scrape_loogika import nimi, hind, transport, toote_url, fetch_html_sisu_ja_soup, pildi_url, salvesta_andmed
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import pandas as pd
 
-def fetch_product_info():
-    url = toode.get()
+pildi_viited = []
+toote_nimed = []
 
+def uuenda_toote_valikumenyy():
+    menyy = toote_valik["menu"]
+    menyy.delete(0, "end")
+    for nimi in toote_nimed:
+        menyy.add_command(label=nimi, command=lambda value=nimi: valitud_toode.set(value))
+
+def hangi_toote_info():
+    global pildi_viited, toote_nimed
+    pildi_viited.clear()
+    toote_nimed.clear()
+    toote_nimi = toote_sisestus.get()
+    for vidin in toote_konteinerid_aken.winfo_children():
+        vidin.destroy()
     try:
-        product_links = toote_url(url)
-
-        for link in product_links:
-            try:
-                html_sisu, soup = fetch_html_sisu_ja_soup(link)
-                hind_value = hind(link, html_sisu, soup)
-                nimi_value = nimi(link, html_sisu, soup)
-                transport_value = transport(link, html_sisu, soup)
-
-                label.config(text=f"{nimi_value}: {hind_value}; {transport_value}")
-            except Exception as e:
-                # Handle the exception (e.g., log it) or simply pass
-                pass
-
+        toote_lingid = toote_url(toote_nimi)
+        for link in toote_lingid:
+            html_sisu, soup = fetch_html_sisu_ja_soup(link)
+            hind_vaartus, ajatempel = hind(link, html_sisu, soup)
+            nimi_vaartus, _ = nimi(link, html_sisu, soup)
+            transport_vaartus, _ = transport(link, html_sisu, soup)
+            pildi_url_väärtus = pildi_url(link, html_sisu, soup)
+            toote_nimed.append(nimi_vaartus)
+            if pildi_url_väärtus and hind_vaartus:
+                toote_konteiner = tk.Frame(toote_konteinerid_aken, bg=taustavärv, borderwidth=0,highlightthickness=0)
+                toote_konteiner.pack(fill=tk.X, pady=10)
+                pildi_andmed = requests.get(pildi_url_väärtus).content
+                pilt = Image.open(io.BytesIO(pildi_andmed))
+                pilt.thumbnail((150, 150))
+                foto = ImageTk.PhotoImage(pilt)
+                pildi_margis = tk.Label(toote_konteiner, image=foto, bg=taustavärv, borderwidth=0,highlightthickness=0)
+                pildi_margis.image = foto
+                pildi_margis.pack(side=tk.LEFT, padx=10)
+                info_tekst = f"{nimi_vaartus}\nHind: {hind_vaartus}\nKohaletoimetamine: {transport_vaartus}"
+                info_margis = tk.Label(toote_konteiner, text=info_tekst, justify=tk.LEFT, bg=taustavärv, fg="White", borderwidth = 0, highlightthickness= 0)
+                info_margis.pack(side=tk.LEFT, padx=10)
+                salvesta_andmed(nimi_vaartus, hind_vaartus, transport_vaartus, ajatempel)
+                pildi_viited.append(foto)
+        uuenda_toote_valikumenyy()
+        toote_aken.update_idletasks()
+        toote_aken.config(scrollregion=toote_aken.bbox("all"))
     except Exception as e:
-        label.config(text=f"Error: {str(e)}")
+        viga_margis.config(text=f"Viga: {str(e)}")
 
+def kuva_graafik(toote_nimi):
+    df = pd.read_csv('toote_ajalugu.csv', names=['Nimi', 'Hind', 'Transport', 'Aeg'])
+    df['Aeg'] = pd.to_datetime(df['Aeg'])
+    df['Hind'] = pd.to_numeric(df['Hind'].replace('[\€\$\,]', '', regex=True), errors='coerce')
+    filtreeritud_df = df[df['Nimi'] == toote_nimi]
+    figuur = Figure(figsize=(5, 4), dpi=100)
+    joonis = figuur.add_subplot(111)
+    joonis.plot(filtreeritud_df['Aeg'], filtreeritud_df['Hind'])
+    graafika_aken = tk.Toplevel(window)
+    graafika_aken.title("Hinnagraafik")
+    canvas = FigureCanvasTkAgg(figuur, master=graafika_aken)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.draw()
 
 window = tk.Tk()
-window.title("Amazonifier 3000")
-window.geometry("800x800")
+taustavärv = "#1D212B"
+tekstivärv = "#d67b04"
+window.title("Amazoni Tooteotsija")
+window.state("zoomed")
+window.configure(bg=taustavärv,borderwidth=0,highlightthickness=0)
 
+sisestuse_aken = tk.Frame(window, bg=taustavärv, borderwidth=0,highlightthickness=0)
+sisestuse_aken.pack(pady=5)
 
-img_label = tk.Label(window)
+toote_sisestus = tk.Entry(sisestuse_aken, width=50)
+toote_sisestus.pack(side=tk.LEFT, padx=10)
 
-toode = tk.Entry(window, width=40)
-toode.insert(0, "")
+toote_aken = tk.Canvas(window, bg=taustavärv, borderwidth=0,highlightthickness=0)
+scrollbar = tk.Scrollbar(window, command=toote_aken.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+toote_aken.config(yscrollcommand=scrollbar.set)
+toote_aken.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-fetch_button = tk.Button(window, text="Leia toote info", command=fetch_product_info)
+toote_konteinerid_aken = tk.Frame(toote_aken, bg=taustavärv, borderwidth=0,highlightthickness=0)
+toote_aken.create_window((0, 0), window=toote_konteinerid_aken, anchor='nw')
 
+graafika_nupp = tk.Button(window, text="Näita graafikut", command=lambda: kuva_graafik(valitud_toode.get()), bg=taustavärv, fg="White", borderwidth=0,highlightthickness=0)
+hangi_nupp = tk.Button(sisestuse_aken, text="Hangi Toote Info", command=hangi_toote_info, bg=taustavärv, fg="white", borderwidth=0,highlightthickness=0)
 
-label = tk.Label(window, text="Toote info")
+viga_margis = tk.Label(window, text="", bg=taustavärv, borderwidth=0,highlightthickness=0)
 
-label.pack(pady=10)
-toode.pack(pady=5)
-fetch_button.pack(pady=10)
-img_label.pack(pady=10)
+valitud_toode = tk.StringVar(window)
+valitud_toode.set("Vali toode")
+toote_valik = tk.OptionMenu(window, valitud_toode, toote_nimed)
+toote_valik.config(bg=taustavärv, fg="white", borderwidth=0,highlightthickness=0)
+
+toote_aken.pack(fill=tk.BOTH, expand=True)
+viga_margis.pack()
+hangi_nupp.pack(side=tk.LEFT, padx=10)
+toote_valik.pack()
+graafika_nupp.pack()
 
 window.mainloop()
--- scrape_loogika.py
+++ scrape_loogika.py
@@ -1,47 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
-import html5lib
-headers = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36", 
-        "Accept-Language": "en-US,en;q=0.9"
-    }
+import lxml
+from datetime import datetime
+import csv
 
 def fetch_html_sisu_ja_soup(url):
-    html_sisu = requests.get(url, headers=headers)
-    soup = BeautifulSoup(html_sisu.text, "html5lib")
+    session = requests.Session()
+    html_sisu = session.get(url, headers=headers)
+    soup = BeautifulSoup(html_sisu.text, "lxml")
     return html_sisu, soup
 
 def hind(url, html_sisu, soup):
-    hind = soup.find(attrs={"a-offscreen"}).get_text()
-    if hind:
-        return hind.strip()
-    else:
-        return "Hinda ei leitud"
-    
+    price_containers = soup.find_all("span", class_="a-price")
+
+    for container in price_containers:
+        price = container.find("span", class_="a-offscreen")
+        if price:
+            return price.get_text().strip(), datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    return "Hinda ei leitud", datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 def transport(url, html_sisu, soup):
-    transport_hind = soup.find(attrs={"a-size-base a-color-secondary"}).get_text()
-    
-    return transport_hind.strip()
+    transport_hind = soup.find(class_={"a-size-base a-color-secondary"}).get_text()
+    return transport_hind.strip(), datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 def nimi(url, html_sisu, soup):
-    nimi = soup.find(id="productTitle").get_text()
-    return nimi.strip()
-    
-    
+    nimi_elem = soup.find("span", id="productTitle")
+    return nimi_elem.get_text().strip(), datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 def toote_url(toode):
     amazon_url = f"https://www.amazon.com/s?k={toode}"
-
     html_sisu, soup = fetch_html_sisu_ja_soup(amazon_url)
-
-    
     lingid = []
-    for product_div in soup.find_all("div", class_="a-section a-spacing-none puis-padding-right-small s-title-instructions-style"):
+
+    product_divs = soup.find_all("div", class_="a-section a-spacing-none puis-padding-right-small s-title-instructions-style")
+    for product_div in product_divs[:10]:
         link = product_div.find("a", class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")
         if link:
             lingid.append(f"https://www.amazon.com{link['href']}")
 
+    if not lingid:
+        product_divs = soup.find_all("div", class_="s-product-image-container aok-relative s-text-center s-image-overlay-grey puis-image-overlay-grey s-padding-left-small s-padding-right-small puis-spacing-small s-height-equalized puis puis-v1tjwq006lh4w12552icvhgwd84")
+        for product_div in product_divs[:10]:
+            link = product_div.find("a", class_="a-link-normal s-no-outline")
+            if link:
+                lingid.append(f"https://www.amazon.com{link['href']}")
+
     return lingid
 
 
+def pildi_url(url, html_sisu, soup):
+    img_tag = soup.find("img", id="landingImage")
+    if img_tag and 'src' in img_tag.attrs:
+        return img_tag['src']
+    else:
+        return None
 
+def salvesta_andmed(toote_nimi, hind, transport_hind, ajatempel):
+    with open('toote_ajalugu.csv', mode='a', newline='', encoding='utf-8') as fail:
+        kirjutaja = csv.writer(fail)
+        kirjutaja.writerow([toote_nimi, hind, transport_hind, ajatempel])
+
+
+print(toote_url("https://www.amazon.com/s?k=versace"))
 
